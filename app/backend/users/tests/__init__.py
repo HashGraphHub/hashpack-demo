fc06b059f5f742fdd4c80ed5121836a9d53e798e
Bobby Stearman <80459294+bobby-didcoding@users.noreply.github.com>
2023-05-05 17:27:11 +0100

    Update __init__.py

--- app/backend/users/tests/__init__.py
+++ app/backend/users/tests/__init__.py
@@ -2,10 +2,12 @@
 # App imports
 # --------------------------------------------------------------
 from users.tests.custom_user import CustomUserTestCase, SignUpTestCase, SignInTestCase
+from users.tests.account import AccountTestCase
 
 
 __all__ = [
     CustomUserTestCase,
     SignUpTestCase, 
-    SignInTestCase
+    SignInTestCase,
+    #AccountTestCase
 ]
