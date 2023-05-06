48f798209442e5bc3e72ced38758aa3f748d9404
Bobby Stearman <80459294+bobby-didcoding@users.noreply.github.com>
2023-05-06 21:09:03 +0100

    Update vite.config.ts

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -2,7 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true,
+        strictPort: true,
+        port: 4173,
+    }
 };
 
 export default config;
